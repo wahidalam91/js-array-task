fb4ce6976202362b7cf9e5b2d0e1fa53e63675a7
AbdelWahid-I <md.wahid104@gmail.com>
2025-08-12 23:20:09 +0600

    Array Task-5.js completed

--- arrayTask-5.js
+++ arrayTask-5.js
@@ -4,3 +4,13 @@
 // a) Create two arrays of your choice.
 // b) Use the concat method to combine the two arrays into a new array.
 // c) Print both the original arrays and the combined array using console.log().
+
+// a
+let officers = ["Wahid", "Sourav", "Imran"];
+let seniorOfficers = ["Mehedy", "Fuad", "Morshed"];
+
+// b
+let teamSreenagar = officers.concat(seniorOfficers);
+
+// c
+console.log(teamSreenagar);
