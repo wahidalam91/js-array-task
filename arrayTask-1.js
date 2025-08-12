0db2ec641b44874f510eb790312a6d67f603d3d7
AbdelWahid-I <md.wahid104@gmail.com>
2025-08-12 21:41:03 +0600

    Array Task -1 completed

---
+++ arrayTask-1.js
@@ -0,0 +1,18 @@
+// Task-1. Declare an array
+// a) Declare an array with 5 elements containing fruits
+// b) console log the 3rd index element
+// c) change the value of the 2nd index element to jambura
+// d) console log the final array
+
+// a
+let fruits = ["Apple", "Bannana", "Cherry", "Dates", "Eggplant"];
+
+// b
+let thirdIndex = fruits[2];
+console.log(thirdIndex);
+
+// c
+fruits[2] = "Jambura";
+
+// d
+console.log(fruits);
