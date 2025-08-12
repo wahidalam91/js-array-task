5e348080877b36aeac87c3f31d8a6717f770e2d1
AbdelWahid-I <md.wahid104@gmail.com>
2025-08-12 21:48:37 +0600

    Array Task-2 completed

---
+++ arrayTask-2.js
@@ -0,0 +1,21 @@
+// Task-2. Add or remove elements
+// a) Declare an array of 3 tourist destinations
+// b) Add a new tourist destination to your tourist array
+// c) Add two more to your array
+// d) Remove the last tourist destination you have added
+// e) display the final array as output
+
+// a
+let touristSpot = ["CoxsBazar", "Sylhet", "Rangamati"];
+
+// b
+touristSpot.push("SaintMartin");
+
+// c
+touristSpot.push("Bandarban", "Sajek");
+
+// d
+touristSpot.pop();
+
+// e
+console.log(touristSpot);
